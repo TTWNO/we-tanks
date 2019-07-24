32986edf27bdd5d344937aa6ad09b11ec695fd21
Tait Hoyem <44244401+TTWNO@users.noreply.github.com>
2019-07-24 00:33:51 +0000

    Add tank gun, point to mouse

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
 #include <stdio.h>
+#include <math.h>
 #include <SFML/Window/Mouse.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Graphics.hpp>
+
+#define PI 3.14159265
 
 int main()
 {
@@ -10,14 +14,14 @@ int main()
     sf::RenderWindow window(sf::VideoMode(300, 300), "SFML works!");
     window.setVerticalSyncEnabled(true);
     const sf::Vector2f size(100.f, 100.f);
+    const sf::Vector2f gun_size(75.f, 25.f);
     sf::RectangleShape shape(size);
-    sf::VertexArray line(sf::Lines, 2);
-    line[0].position = shape.getPosition();
-    line[0].color = sf::Color::Cyan;
-    sf::Vector2i mouse_pos = sf::Mouse::getPosition();
-    line[1].position = sf::Vector2f((float)mouse_pos.x, (float)mouse_pos.y);
-    line[1].color = sf::Color::Cyan;
+    sf::RectangleShape gun_shape(gun_size);
+
+    shape.setFillColor(sf::Color::Blue);
     shape.setFillColor(sf::Color::Red);
+    gun_shape.setPosition(50.f, 50.f);
+    gun_shape.setOrigin(0.f, 12.5f);
 
     while (window.isOpen()){
         sf::Vector2u size = window.getSize();
@@ -31,25 +35,29 @@ int main()
         // Keyboard events
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
             shape.move(velocity, 0);
+            gun_shape.move(velocity, 0);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
             shape.move(-velocity, 0);
+            gun_shape.move(-velocity, 0);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
             shape.move(0, -velocity);
+            gun_shape.move(0, -velocity);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
             shape.move(0, velocity);
+            gun_shape.move(0, velocity);
         }
 
-        auto sp = shape.getPosition();
-        line[0].position = sf::Vector2f(sp.x + (shape.getSize().x/2), sp.y + (shape.getSize().y/2));
-        mouse_pos = sf::Mouse::getPosition(window);
-        line[1].position = sf::Vector2f((float)mouse_pos.x, (float)mouse_pos.y);
+        sf::Vector2i mp = sf::Mouse::getPosition(window);
+        float diff3 = atan2((float)mp.y - gun_shape.getPosition().y,
+                            (float)mp.x - gun_shape.getPosition().x) * 180 / PI;
+        gun_shape.setRotation(diff3);
 
         window.clear();
         window.draw(shape);
-        window.draw(line);
+        window.draw(gun_shape);
         window.display();
     }
 

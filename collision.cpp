b6fb7f4e97f7d3028ba77126b48b386327239e8f
Tait Hoyem <44244401+TTWNO@users.noreply.github.com>
2019-07-27 17:03:33 +0000

    SFML1 collision -> SFML2 collision

--- collision.cpp
+++ collision.cpp
@@ -1,273 +1,190 @@
 /* 
  * File:   collision.cpp
- * Author: Nick
- * 
- * Created on 30 January 2009, 11:02
+ * Author: Nick (original version), ahnonay (SFML2 compatibility)
  */
 #include <SFML/Graphics.hpp>
-#include "collision.h"
+#include <map>
+#include "Collision.h"
+
+namespace Collision
+{
+	class BitmaskManager
+	{
+	public:
+		~BitmaskManager() {
+			std::map<const sf::Texture*, sf::Uint8*>::const_iterator end = Bitmasks.end();
+			for (std::map<const sf::Texture*, sf::Uint8*>::const_iterator iter = Bitmasks.begin(); iter!=end; iter++)
+				delete [] iter->second;
+		}
+
+		sf::Uint8 GetPixel (const sf::Uint8* mask, const sf::Texture* tex, unsigned int x, unsigned int y) {
+			if (x>tex->getSize().x||y>tex->getSize().y)
+				return 0;
+
+			return mask[x+y*tex->getSize().x];
+		}
+
+		sf::Uint8* GetMask (const sf::Texture* tex) {
+			sf::Uint8* mask;
+			std::map<const sf::Texture*, sf::Uint8*>::iterator pair = Bitmasks.find(tex);
+			if (pair==Bitmasks.end())
+			{
+				sf::Image img = tex->copyToImage();
+				mask = CreateMask (tex, img);
+			}
+			else
+				mask = pair->second;
+
+			return mask;
+		}
+
+		sf::Uint8* CreateMask (const sf::Texture* tex, const sf::Image& img) {
+			sf::Uint8* mask = new sf::Uint8[tex->getSize().y*tex->getSize().x];
+
+			for (unsigned int y = 0; y<tex->getSize().y; y++)
+			{
+				for (unsigned int x = 0; x<tex->getSize().x; x++)
+					mask[x+y*tex->getSize().x] = img.getPixel(x,y).a;
+			}
+
+			Bitmasks.insert(std::pair<const sf::Texture*, sf::Uint8*>(tex,mask));
+
+			return mask;
+		}
+	private:
+		std::map<const sf::Texture*, sf::Uint8*> Bitmasks;
+	};
+	
+	BitmaskManager Bitmasks;
  
-Collision::Collision() {
-}
+	bool PixelPerfectTest(const sf::Sprite& Object1, const sf::Sprite& Object2, sf::Uint8 AlphaLimit) {
+		sf::FloatRect Intersection; 
+		if (Object1.getGlobalBounds().intersects(Object2.getGlobalBounds(), Intersection)) {
+			sf::IntRect O1SubRect = Object1.getTextureRect();
+			sf::IntRect O2SubRect = Object2.getTextureRect();
+
+			sf::Uint8* mask1 = Bitmasks.GetMask(Object1.getTexture());
+			sf::Uint8* mask2 = Bitmasks.GetMask(Object2.getTexture());
+
+			// Loop through our pixels
+			for (int i = Intersection.left; i < Intersection.left+Intersection.width; i++) {
+				for (int j = Intersection.top; j < Intersection.top+Intersection.height; j++) {
  
-Collision::~Collision() {
-}
-/* 
-sf::IntRect Collision::GetAABB(const sf::Sprite& Object) {
+					sf::Vector2f o1v = Object1.getInverseTransform().transformPoint(i, j);
+					sf::Vector2f o2v = Object2.getInverseTransform().transformPoint(i, j);
  
-    //Get the top left corner of the sprite regardless of the sprite's center
-    //This is in Global Coordinates so we can put the rectangle back into the right place
-    sf::Vector2f pos = Object.TransformToGlobal(sf::Vector2f(0, 0));
- 
-    //Store the size so we can calculate the other corners
-    sf::Vector2f size = Object.GetSize();
- 
-    float Angle = Object.GetRotation();
- 
-    //Bail out early if the sprite isn't rotated
-    if (Angle == 0.0f) {
-        return sf::IntRect(static_cast<int> (pos.x),
-                static_cast<int> (pos.y),
-                static_cast<int> (pos.x + size.x),
-                static_cast<int> (pos.y + size.y));
-    }
- 
-    //Calculate the other points as vectors from (0,0)
-    //Imagine sf::Vector2f A(0,0); but its not necessary
-    //as rotation is around this point.
-    sf::Vector2f B(size.x, 0);
-    sf::Vector2f C(size.x, size.y);
-    sf::Vector2f D(0, size.y);
- 
-    //Rotate the points to match the sprite rotation
-    B = RotatePoint(B, Angle);
-    C = RotatePoint(C, Angle);
-    D = RotatePoint(D, Angle);
- 
-    //Round off to int and set the four corners of our Rect
-    int Left = static_cast<int> (MinValue(0.0f, B.x, C.x, D.x));
-    int Top = static_cast<int> (MinValue(0.0f, B.y, C.y, D.y));
-    int Right = static_cast<int> (MaxValue(0.0f, B.x, C.x, D.x));
-    int Bottom = static_cast<int> (MaxValue(0.0f, B.y, C.y, D.y));
- 
-    //Create a Rect from out points and move it back to the correct position on the screen
-    sf::IntRect AABB = sf::IntRect(Left, Top, Right, Bottom);
-    AABB.Offset(static_cast<int> (pos.x), static_cast<int> (pos.y));
-    return AABB;
-}
- 
-float Collision::MinValue(float a, float b, float c, float d) {
-    float min = a;
- 
-    min = (b < min ? b : min);
-    min = (c < min ? c : min);
-    min = (d < min ? d : min);
- 
-    return min;
-}
- 
-float Collision::MaxValue(float a, float b, float c, float d) {
-    float max = a;
- 
-    max = (b > max ? b : max);
-    max = (c > max ? c : max);
-    max = (d > max ? d : max);
- 
-    return max;
-}
-*/ 
-sf::Vector2f Collision::RotatePoint(const sf::Vector2f& Point, float Angle) {
-    Angle = Angle * RADIANS_PER_DEGREE;
-    sf::Vector2f RotatedPoint;
-    RotatedPoint.x = Point.x * cos(Angle) + Point.y * sin(Angle);
-    RotatedPoint.y = -Point.x * sin(Angle) + Point.y * cos(Angle);
-    return RotatedPoint;
-}
-/* 
-bool Collision::PixelPerfectTest(const sf::Sprite& Object1, const sf::Sprite& Object2, sf::Uint8 AlphaLimit) {
-        //Get AABBs of the two sprites
-    sf::IntRect Object1AABB = GetAABB(Object1);
-    sf::IntRect Object2AABB = GetAABB(Object2);
- 
-    sf::IntRect Intersection;
- 
-    if (Object1AABB.Intersects(Object2AABB, &Intersection)) {
- 
-        //We've got an intersection we need to process the pixels
-        //In that Rect.
- 
-        //Bail out now if AlphaLimit = 0
-        if (AlphaLimit == 0) return true;
- 
-        //There are a few hacks here, sometimes the TransformToLocal returns negative points
-        //Or Points outside the image.  We need to check for these as they print to the error console
-        //which is slow, and then return black which registers as a hit.
- 
-        sf::IntRect O1SubRect = Object1.GetSubRect();
-        sf::IntRect O2SubRect = Object2.GetSubRect();
- 
-        sf::Vector2i O1SubRectSize(O1SubRect.GetWidth(), O1SubRect.GetHeight());
-        sf::Vector2i O2SubRectSize(O2SubRect.GetWidth(), O2SubRect.GetHeight());
- 
-        sf::Vector2f o1v;
-        sf::Vector2f o2v;
-        //Loop through our pixels
-        for (int i = Intersection.Left; i < Intersection.Right; i++) {
-            for (int j = Intersection.Top; j < Intersection.Bottom; j++) {
- 
-                o1v = Object1.TransformToLocal(sf::Vector2f(i, j)); //Creating Objects each loop :(
-                o2v = Object2.TransformToLocal(sf::Vector2f(i, j));
- 
-                //Hack to make sure pixels fall withint the Sprite's Image
-                if (o1v.x > 0 && o1v.y > 0 && o2v.x > 0 && o2v.y > 0 &&
-                        o1v.x < O1SubRectSize.x && o1v.y < O1SubRectSize.y &&
-                        o2v.x < O2SubRectSize.x && o2v.y < O2SubRectSize.y) {
- 
-                    //If both sprites have opaque pixels at the same point we've got a hit
-                    if ((Object1.GetPixel(static_cast<int> (o1v.x), static_cast<int> (o1v.y)).a > AlphaLimit) &&
-                            (Object2.GetPixel(static_cast<int> (o2v.x), static_cast<int> (o2v.y)).a > AlphaLimit)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-    return false;
-}
- 
-bool Collision::CircleTest(const sf::Sprite& Object1, const sf::Sprite& Object2) {
-    //Simplest circle test possible
-    //Distance between points <= sum of radius
- 
-    float Radius1 = (Object1.GetSize().x + Object1.GetSize().y) / 4;
-    float Radius2 = (Object2.GetSize().x + Object2.GetSize().y) / 4;
-    float xd = Object1.GetPosition().x - Object2.GetPosition().x;
-    float yd = Object1.GetPosition().y - Object2.GetPosition().y;
- 
-    return sqrt(xd * xd + yd * yd) <= Radius1 + Radius2;
-}
- */
-//From Rotated Rectangles Collision Detection, Oren Becker, 2001
- 
-bool Collision::BoundingBoxTest(const sf::RectangleShape& Object1, const sf::RectangleShape& Object2) {
- 
-    sf::Vector2f A, B, C, BL, TR;
-    sf::Vector2f HalfSize1 = Object1.getSize();
-    sf::Vector2f HalfSize2 = Object2.getSize();
- 
-    //For somereason the Vector2d divide by operator
-    //was misbehaving
-    //Doing it manually
-    HalfSize1.x /= 2;
-    HalfSize1.y /= 2;
-    HalfSize2.x /= 2;
-    HalfSize2.y /= 2;
-    //get the Angle we're working on
-    float Angle = Object1.getRotation() - Object2.getRotation();
-    float CosA = cos(Angle * RADIANS_PER_DEGREE);
-    float SinA = sin(Angle * RADIANS_PER_DEGREE);
- 
-    float t, x, a, dx, ext1, ext2;
- 
-    //Normalise the Center of Object2 so its axis aligned an represented in
-    //relation to Object 1
-    C = Object2.getPosition();
- 
-    C -= Object1.getPosition();
- 
-    C = RotatePoint(C, Object2.getRotation());
- 
-    //Get the Corners
-    BL = TR = C;
-    BL -= HalfSize2;
-    TR += HalfSize2;
- 
-    //Calculate the vertices of the rotate Rect
-    A.x = -HalfSize1.y*SinA;
-    B.x = A.x;
-    t = HalfSize1.x*CosA;
-    A.x += t;
-    B.x -= t;
- 
-    A.y = HalfSize1.y*CosA;
-    B.y = A.y;
-    t = HalfSize1.x*SinA;
-    A.y += t;
-    B.y -= t;
- 
-    t = SinA * CosA;
- 
-    // verify that A is vertical min/max, B is horizontal min/max
-    if (t < 0) {
-        t = A.x;
-        A.x = B.x;
-        B.x = t;
-        t = A.y;
-        A.y = B.y;
-        B.y = t;
-    }
- 
-    // verify that B is horizontal minimum (leftest-vertex)
-    if (SinA < 0) {
-        B.x = -B.x;
-        B.y = -B.y;
-    }
- 
-    // if rr2(ma) isn't in the horizontal range of
-    // colliding with rr1(r), collision is impossible
-    if (B.x > TR.x || B.x > -BL.x) return false;
- 
-    // if rr1(r) is axis-aligned, vertical min/max are easy to get
-    if (t == 0) {
-        ext1 = A.y;
-        ext2 = -ext1;
-    }// else, find vertical min/max in the range [BL.x, TR.x]
-    else {
-        x = BL.x - A.x;
-        a = TR.x - A.x;
-        ext1 = A.y;
-        // if the first vertical min/max isn't in (BL.x, TR.x), then
-        // find the vertical min/max on BL.x or on TR.x
-        if (a * x > 0) {
-            dx = A.x;
-            if (x < 0) {
-                dx -= B.x;
-                ext1 -= B.y;
-                x = a;
-            } else {
-                dx += B.x;
-                ext1 += B.y;
-            }
-            ext1 *= x;
-            ext1 /= dx;
-            ext1 += A.y;
-        }
- 
-        x = BL.x + A.x;
-        a = TR.x + A.x;
-        ext2 = -A.y;
-        // if the second vertical min/max isn't in (BL.x, TR.x), then
-        // find the local vertical min/max on BL.x or on TR.x
-        if (a * x > 0) {
-            dx = -A.x;
-            if (x < 0) {
-                dx -= B.x;
-                ext2 -= B.y;
-                x = a;
-            } else {
-                dx += B.x;
-                ext2 += B.y;
-            }
-            ext2 *= x;
-            ext2 /= dx;
-            ext2 -= A.y;
-        }
-    }
- 
-    // check whether rr2(ma) is in the vertical range of colliding with rr1(r)
-    // (for the horizontal range of rr2)
-    return !((ext1 < BL.y && ext2 < BL.y) ||
-            (ext1 > TR.y && ext2 > TR.y));
- 
+					// Make sure pixels fall within the sprite's subrect
+					if (o1v.x > 0 && o1v.y > 0 && o2v.x > 0 && o2v.y > 0 &&
+						o1v.x < O1SubRect.width && o1v.y < O1SubRect.height &&
+						o2v.x < O2SubRect.width && o2v.y < O2SubRect.height) {
+
+							if (Bitmasks.GetPixel(mask1, Object1.getTexture(), (int)(o1v.x)+O1SubRect.left, (int)(o1v.y)+O1SubRect.top) > AlphaLimit &&
+								Bitmasks.GetPixel(mask2, Object2.getTexture(), (int)(o2v.x)+O2SubRect.left, (int)(o2v.y)+O2SubRect.top) > AlphaLimit)
+								return true;
+
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	bool CreateTextureAndBitmask(sf::Texture &LoadInto, const std::string& Filename)
+	{
+		sf::Image img;
+		if (!img.loadFromFile(Filename))
+			return false;
+		if (!LoadInto.loadFromImage(img))
+			return false;
+
+		Bitmasks.CreateMask(&LoadInto, img);
+		return true;
+	}
+
+	sf::Vector2f GetSpriteCenter (const sf::Sprite& Object)
+	{
+		sf::FloatRect AABB = Object.getGlobalBounds();
+		return sf::Vector2f (AABB.left+AABB.width/2.f, AABB.top+AABB.height/2.f);
+	}
+
+	sf::Vector2f GetSpriteSize (const sf::Sprite& Object)
+	{
+		sf::IntRect OriginalSize = Object.getTextureRect();
+		sf::Vector2f Scale = Object.getScale();
+		return sf::Vector2f (OriginalSize.width*Scale.x, OriginalSize.height*Scale.y);
+	}
+
+	bool CircleTest(const sf::Sprite& Object1, const sf::Sprite& Object2) {
+		sf::Vector2f Obj1Size = GetSpriteSize(Object1);
+		sf::Vector2f Obj2Size = GetSpriteSize(Object2);
+		float Radius1 = (Obj1Size.x + Obj1Size.y) / 4;
+		float Radius2 = (Obj2Size.x + Obj2Size.y) / 4;
+
+		sf::Vector2f Distance = GetSpriteCenter(Object1)-GetSpriteCenter(Object2);
+
+		return (Distance.x * Distance.x + Distance.y * Distance.y <= (Radius1 + Radius2) * (Radius1 + Radius2));
+	}
+
+	class OrientedBoundingBox // Used in the BoundingBoxTest
+	{
+	public:
+		OrientedBoundingBox (const sf::Sprite& Object) // Calculate the four points of the OBB from a transformed (scaled, rotated...) sprite
+		{
+			sf::Transform trans = Object.getTransform();
+			sf::IntRect local = Object.getTextureRect();
+			Points[0] = trans.transformPoint(0.f, 0.f);
+			Points[1] = trans.transformPoint(local.width, 0.f);
+			Points[2] = trans.transformPoint(local.width, local.height);
+			Points[3] = trans.transformPoint(0.f, local.height);
+		}
+
+		sf::Vector2f Points[4];
+
+		void ProjectOntoAxis (const sf::Vector2f& Axis, float& Min, float& Max) // Project all four points of the OBB onto the given axis and return the dotproducts of the two outermost points
+		{
+			Min = (Points[0].x*Axis.x+Points[0].y*Axis.y);
+			Max = Min;
+			for (int j = 1; j<4; j++)
+			{
+				float Projection = (Points[j].x*Axis.x+Points[j].y*Axis.y);
+
+				if (Projection<Min)
+					Min=Projection;
+				if (Projection>Max)
+					Max=Projection;
+			}
+		}
+	};
+
+	bool BoundingBoxTest(const sf::Sprite& Object1, const sf::Sprite& Object2) {
+		OrientedBoundingBox OBB1 (Object1);
+		OrientedBoundingBox OBB2 (Object2);
+
+		// Create the four distinct axes that are perpendicular to the edges of the two rectangles
+		sf::Vector2f Axes[4] = {
+			sf::Vector2f (OBB1.Points[1].x-OBB1.Points[0].x,
+			OBB1.Points[1].y-OBB1.Points[0].y),
+			sf::Vector2f (OBB1.Points[1].x-OBB1.Points[2].x,
+			OBB1.Points[1].y-OBB1.Points[2].y),
+			sf::Vector2f (OBB2.Points[0].x-OBB2.Points[3].x,
+			OBB2.Points[0].y-OBB2.Points[3].y),
+			sf::Vector2f (OBB2.Points[0].x-OBB2.Points[1].x,
+			OBB2.Points[0].y-OBB2.Points[1].y)
+		};
+
+		for (int i = 0; i<4; i++) // For each axis...
+		{
+			float MinOBB1, MaxOBB1, MinOBB2, MaxOBB2;
+
+			// ... project the points of both OBBs onto the axis ...
+			OBB1.ProjectOntoAxis(Axes[i], MinOBB1, MaxOBB1);
+			OBB2.ProjectOntoAxis(Axes[i], MinOBB2, MaxOBB2);
+
+			// ... and check whether the outermost projected points of both OBBs overlap.
+			// If this is not the case, the Separating Axis Theorem states that there can be no collision between the rectangles
+			if (!((MinOBB2<=MaxOBB1)&&(MaxOBB2>=MinOBB1)))
+				return false;
+		}
+		return true;
+	}
 }
--- collision.h
+++ collision.h
@@ -1,10 +1,12 @@
 /* 
  * File:   collision.h
- * Author: Nick Koirala
+ * Authors: Nick Koirala (original version), ahnonay (SFML2 compatibility)
  *
  * Collision Detection and handling class
- * For SFML.
+ * For SFML2.
  
+Notice from the original version:
+
 (c) 2009 - LittleMonkey Ltd
  
 This software is provided 'as-is', without any express or
@@ -30,78 +32,46 @@ it freely, subject to the following restrictions:
  *
  * Created on 30 January 2009, 11:02
  */
+
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include <SFML/Graphics.hpp>
+
+namespace Collision {
+	//////
+	/// Test for a collision between two sprites by comparing the alpha values of overlapping pixels
+	/// Supports scaling and rotation
+	/// AlphaLimit: The threshold at which a pixel becomes "solid". If AlphaLimit is 127, a pixel with
+	/// alpha value 128 will cause a collision and a pixel with alpha value 126 will not.
+	/// 
+	/// This functions creates bitmasks of the textures of the two sprites by
+	/// downloading the textures from the graphics card to memory -> SLOW!
+	/// You can avoid this by using the "CreateTextureAndBitmask" function
+	//////
+	bool PixelPerfectTest(const sf::Sprite& Object1 ,const sf::Sprite& Object2, sf::Uint8 AlphaLimit = 0);
+
+	//////
+	/// Replaces Texture::loadFromFile
+	/// Load an imagefile into the given texture and create a bitmask for it
+	/// This is much faster than creating the bitmask for a texture on the first run of "PixelPerfectTest"
+	/// 
+	/// The function returns false if the file could not be opened for some reason
+	//////
+	bool CreateTextureAndBitmask(sf::Texture &LoadInto, const std::string& Filename);
  
-#ifndef _COLLISION_H
-#define	_COLLISION_H
+	//////
+	/// Test for collision using circle collision dection
+	/// Radius is averaged from the dimensions of the sprite so
+	/// roughly circular objects will be much more accurate
+	//////
+	bool CircleTest(const sf::Sprite& Object1, const sf::Sprite& Object2);
  
- 
-#ifndef PI
-    #define PI (3.14159265358979323846)
-#endif
-#define RADIANS_PER_DEGREE (PI/180.0)
- 
- 
-class Collision {
-public:
- 
-    virtual ~Collision();
- 
-    /**
-     *  Test for a pixel perfect collision detection between
-     *  two Sprites, Rotation and Scale is supported in this test
-     *
-     *  @param Object1 The first sprite
-     *  @param Object2 The second sprite
-     *  @AlphaLimit How opaque a pixel needs to be before a hit it registered
-     */
-     static bool PixelPerfectTest(const sf::Sprite& Object1 ,const sf::Sprite& Object2, sf::Uint8 AlphaLimit = 127);
- 
-    /**
-     *  Test for collision using circle collision dection
-     *  Radius is averaged from the dimesnions of the sprite so
-     *  roughly circular objects will be much more accurate
-     */
-    static bool CircleTest(const sf::Sprite& Object1, const sf::Sprite& Object2);
- 
-    /**
-     *  Test for bounding box collision using OBB Box.
-     *  To test against AABB use PixelPerfectTest with AlphaLimit = 0
-     *
-     *  @see Collision::PixelPerfectTest
-     */
-    static bool BoundingBoxTest(const sf::RectangleShape& Object1, const sf::RectangleShape& Object2);
- 
-    /**
-     *  Generate a Axis-Aligned Bounding Box for broad phase of
-     *  Pixel Perfect detection.
-     *
-     *  @returns IntRect to round off Floating point positions.
-     */
-    static sf::IntRect GetAABB(const sf::Sprite& Object);
- 
-    /**
-     *  Helper function in order to rotate a point by an Angle
-     *
-     *  Rotation is CounterClockwise in order to match SFML Sprite Rotation
-     *
-     *  @param Point a Vector2f representing a coordinate
-     *  @param Angle angle in degrees
-     */
-    static sf::Vector2f RotatePoint(const sf::Vector2f& Point, float Angle);
- 
-    /**
-     *  Helper function to get the minimum from a list of values
-     */
-    static float MinValue(float a, float b, float c, float d);
- 
-    /**
-     *  Helper function to get the maximum from a list of values
-     */
-    static float MaxValue(float a, float b, float c, float d);
- 
-private:
- 
-    Collision();
-};
- 
-#endif	/* _COLLISION_H */
+	//////
+	/// Test for bounding box collision using the Separating Axis Theorem
+	/// Supports scaling and rotation
+	//////
+	bool BoundingBoxTest(const sf::Sprite& Object1, const sf::Sprite& Object2);
+}
+
+#endif	/* COLLISION_H */
